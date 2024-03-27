4d986567a5f4e20ef014d7f951df43369b937abc
Amit Chaudhary <itheamc@gmail.com>
2024-03-27 17:24:14 +0545

    updated for latest flutter version >=3.16.9

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\User\Documents\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\Forked Plugins\dio-http-cache\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\User\Desktop\dio-http-cache\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
